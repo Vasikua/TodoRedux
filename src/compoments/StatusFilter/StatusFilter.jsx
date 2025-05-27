d6410324653889b5e52df44134fe7254dd2fc185
vasik_ua <vasik_ua@ukr.net>
2025-05-26 21:59:51 -0500

    add redux store

---
+++ src/compoments/StatusFilter/StatusFilter.jsx
@@ -0,0 +1,15 @@
+import { Button } from '../Button/Button';
+import css from './StatusFilter.module.css';
+import { useSelector } from 'react-redux';
+
+export const StatusFilter = () => {
+    const filter = useSelector(state => state.filters.status);
+
+    return (
+    <div className={css.wrapper}>
+      <Button>All{filter === "all" && "is active" }</Button>
+      <Button>Active{filter === "active" && "is active" }</Button>
+      <Button>Completed{filter === "completed" && "is active" }</Button>
+    </div>
+  );
+};
